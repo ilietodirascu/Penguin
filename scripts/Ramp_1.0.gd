0c1aad78f4df519bdc93a40da5494154a4a9298b
Radu <99242964+RaduDobrovolschi@users.noreply.github.com>
2024-02-11 17:28:42 +0200

    made the penguine slide a little

--- scripts/Ramp_1.0.gd
+++ scripts/Ramp_1.0.gd
@@ -9,4 +9,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	ResourceSaver.save(Resource.new(), "res://materials/icy_surface.tres")
