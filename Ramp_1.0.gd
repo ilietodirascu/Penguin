f2c0dc741b54838d2d90c6d21cc88df6ca3620f4
Radu <99242964+RaduDobrovolschi@users.noreply.github.com>
2024-02-11 16:51:10 +0200

    working on the scene

---
+++ Ramp_1.0.gd
@@ -0,0 +1,12 @@
+extends Polygon2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var colission_shape = CollisionPolygon2D.new()
+	colission_shape.polygon = polygon
+	$StaticBody2D.add_child(colission_shape)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
