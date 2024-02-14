d1d570fe2f4282fc6c6907b2183d7ef376cd4579
ilietodirascu <ilietodirascu@gmail.com>
2024-02-14 16:26:53 +0200

    ( ͡° ͜ʖ ͡°)

--- scripts/Penguin.gd
+++ scripts/Penguin.gd
@@ -1,28 +1,17 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var gravity = Vector2(0, 500)  # Adjust the gravity vector as needed
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	# Apply gravity
+	velocity += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Assuming the CharacterBody2D is already set to 'floating' mode
+	# which is appropriate for icy conditions without distinct floor/wall/ceiling
+	motion_mode = MOTION_MODE_FLOATING
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# The `velocity` property will be automatically used by `move_and_slide`
 	move_and_slide()
+	
+	# After move_and_slide, the `velocity` property is automatically updated
+	# by Godot to reflect the new velocity after the physics step.
